f27805799701c6ed0f3e4dcdf722cf4090968ea1
perklembo <91026899+perklembo@users.noreply.github.com>
2021-11-04 10:52:18 +0100

    Initial commit

---
+++ MuseumTemplate/Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+    namespace MuseumTemplate.Models
+    {
+        public class Booking
+        {
+            public int ID { get; set; }
+
+            [DataType(DataType.DateTime)]
+            public DateTime Time { get; set; }
+
+            public int ParticipantCount { get; set; }
+
+            public string GroupName { get; set; }
+        }
+    }
